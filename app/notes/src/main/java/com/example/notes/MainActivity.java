3022478c6baf957cec1cf3c79d9790835c637ca6
belenkotv <unamunatara@mail.ru>
2021-09-13 07:49:02 +0300

    hw #7

--- app/notes/src/main/java/com/example/notes/MainActivity.java
+++ app/notes/src/main/java/com/example/notes/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.notes;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         ITEMS.add(new Note("Заметка №3", "вообще ничего особенного"));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
         int orientation = this.getApplicationContext().getResources().getConfiguration().orientation;
         if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
             showNoteFragment(0);
@@ -31,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
             findViewById(R.id.line).setVisibility(View.GONE);
             findViewById(R.id.note).setVisibility(View.GONE);
         }
+    }@Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     public void showNoteFragment(int position) {
@@ -41,6 +55,37 @@ public class MainActivity extends AppCompatActivity {
         //ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         ft.addToBackStack(null);
         ft.commit();
+    }
+
+    private void initViews() {
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final DrawerLayout drawer = findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.open,
+                R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int id = item.getItemId();
+                if (navigateFragment(id)){
+                    drawer.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private boolean navigateFragment(int id) {
+        return true;
     }
 
 }
