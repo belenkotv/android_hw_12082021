6d66bfcf1ee9d25f4177f9600faa7bd8ece18c2d
belenkotv <unamunatara@mail.ru>
2021-09-16 18:50:55 +0300

    hw #9

--- app/notes/src/main/java/com/example/notes/NoteShortFragment.java
+++ app/notes/src/main/java/com/example/notes/NoteShortFragment.java
@@ -1,21 +1,39 @@
 package com.example.notes;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 /**
  * A fragment representing a list of Items.
  */
-public class NoteShortFragment extends Fragment {
+public class NoteShortFragment extends Fragment implements NoteRecyclerViewAdapter.NoteClickListener {
+
+    public static final String RESULT = "result";
+    public static final String POSITION = "position";
+
+    private RecyclerView list;
+    private ActivityResultLauncher<Intent> launcher;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -25,15 +43,87 @@ public class NoteShortFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        launcher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        list.getAdapter().notifyDataSetChanged();
+                    }
+                }
+            });
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_note_short_list, container, false);
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            recyclerView.setAdapter(new NoteRecyclerViewAdapter(MainActivity.ITEMS));
+            list = (RecyclerView) view;
+            list.setLayoutManager(new LinearLayoutManager(context));
+            list.setAdapter(new NoteRecyclerViewAdapter(MainActivity.ITEMS, this));
         }
         return view;
     }
+
+    @Override
+    public void onClick(int position) {
+        if (position < 0) {
+            launchNoteActivity(position, true);
+            return;
+        }
+        int orientation = getContext().getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Bundle bundle = new Bundle();
+            bundle.putInt(POSITION, position);
+            getParentFragmentManager().setFragmentResult(RESULT, bundle);
+        } else {
+            launchNoteActivity(position, false);
+        }
+    }
+
+    @Override
+    public void onLongClick(int position, View view) {
+        if (position < 0) {
+            return;
+        }
+        PopupMenu popup = new PopupMenu(view.getContext(), view);
+        popup.getMenuInflater().inflate(R.menu.menu_context, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.edit:
+                        launchNoteActivity(position, true);
+                        break;
+                    case R.id.delete:
+                        new AlertDialog.Builder(view.getContext())
+                            .setTitle(R.string.delete)
+                            .setMessage(R.string.delete_note)
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    MainActivity.ITEMS.remove(position);
+                                    list.getAdapter().notifyDataSetChanged();
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, null)
+                            .show();
+                        break;
+                }
+                return true;
+            }
+        });
+        popup.show();
+    }
+
+    private void launchNoteActivity(int position, boolean forEdit) {
+        Intent intent = new Intent(getContext(), NoteActivity.class);
+        intent.putExtra(NoteActivity.INDEX, position);
+        intent.putExtra(NoteActivity.EDIT, forEdit);
+        launcher.launch(intent);
+    }
+
 }
---
+++ app/notes/src/main/java/com/example/notes/SettingsFragment.java
@@ -0,0 +1,26 @@
+package com.example.notes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import java.text.SimpleDateFormat;
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return (ViewGroup)inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+}
